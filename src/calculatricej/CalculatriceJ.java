8bc04ec72a9e5630bfc46d65af7b8c35897c0a86
Anama08 <anamaurrealara@hotmail.com>
2021-12-01 11:28:55 +0100

    init

---
+++ src/calculatricej/CalculatriceJ.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculatricej;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author anama
+ */
+public class CalculatriceJ {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here? 
+        /*int numero1;
+        int numero2;
+        int resultat;
+        Scanner scanner= new Scanner(System.in);
+        System.out.println("premier numero");
+        numero1=scanner.nextInt();
+
+        System.out.println("secound numero");
+        numero2=scanner.nextInt();
+
+        resultat = numero1+numero2;
+        System.out.println("l'adicion est"+resultat);*/
+
+        
+    }
+    
+}
